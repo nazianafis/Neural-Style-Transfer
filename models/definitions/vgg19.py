ad9f24844c863f4f44c49393b582b8bfd373e8f5
Nazia Nafis <81024656+nazianafis@users.noreply.github.com>
2021-12-18 18:15:05 +0530

    Update vgg19.py

--- models/definitions/vgg19.py
+++ models/definitions/vgg19.py
@@ -4,8 +4,8 @@ from torchvision import models
 
 class Vgg19(torch.nn.Module):
     """
-    'conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1' are used for style representation
-    'conv4_2' is used for content representation
+    VGG19 has a total of 19 layers. Out of them, 'conv4_2' is used for content representation,
+    and 'conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1' are used for style representation.
     """
     def __init__(self, requires_grad=False, show_progress=False, use_relu=True):
         super().__init__()
@@ -13,9 +13,9 @@ class Vgg19(torch.nn.Module):
 
         self.layer_names = ['relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'conv4_2', 'relu5_1']
         self.offset = 1
-        self.content_feature_maps_index = 4  # conv4_2
+        self.content_feature_maps_index = 4 
         self.style_feature_maps_indices = list(range(len(self.layer_names)))
-        self.style_feature_maps_indices.remove(4)  # conv4_2
+        self.style_feature_maps_indices.remove(4)
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
         self.slice3 = torch.nn.Sequential()
@@ -35,6 +35,7 @@ class Vgg19(torch.nn.Module):
             self.slice5.add_module(str(x), vgg_pretrained_features[x])
         for x in range(22, 29++self.offset):
             self.slice6.add_module(str(x), vgg_pretrained_features[x])
+            
         if not requires_grad:
             for param in self.parameters():
                 param.requires_grad = False
